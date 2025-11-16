7961babc50b74ad53631295d322f196aa04594b4
HairyDan <danielschormans@googlemail.com>
2025-11-16 19:34:45 +0000

    spring boot setup

---
+++ src/main/java/com/heliosx/genovia/GenoviaApplication.java
@@ -0,0 +1,13 @@
+package com.heliosx.genovia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenoviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenoviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heliosx/genovia/GenoviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heliosx.genovia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenoviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
